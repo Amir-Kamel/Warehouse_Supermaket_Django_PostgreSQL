cdcfe43970044f34ba4d3624e25d6cfda29f35b9
amir-kamel <amir.kghabrial@gmail.com>
2025-03-23 21:10:26 +0200

    update for railway app deployment

--- TrackStock/TrackStock/settings.py
+++ TrackStock/TrackStock/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,11 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i7z2nwp&com&awl0g5ol6$it@%s5-*laxpztp)g$6v76y%-won'
+
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-i7z2nwp&com&awl0g5ol6$it@%s5-*laxpztp)g$6v76y%-won")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
-ALLOWED_HOSTS = []
+
+DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
+
+
+ALLOWED_HOSTS = ["supermarket-warehouse-inventory.up.railway.app", "127.0.0.1"]
+
+
 
 
 # Application definition
@@ -59,7 +67,7 @@ ROOT_URLCONF = 'TrackStock.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-         'DIRS': [os.path.join(BASE_DIR,"TrackStock" ,"templates")],  
+         'DIRS': [os.path.join(BASE_DIR,"TrackStock" ,"templates")],
                  'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -85,6 +93,7 @@ WSGI_APPLICATION = 'TrackStock.wsgi.application'
 #     }
 # }
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -95,6 +104,13 @@ DATABASES = {
         "PORT": "5432",
     }
 }
+
+# Use Neon database if running in a production environment
+NEON_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if NEON_DATABASE_URL:  # If DATABASE_URL is set, switch to Neon DB
+    DATABASES["default"] = dj_database_url.config(default=NEON_DATABASE_URL)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -130,19 +146,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Static files
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+if not os.path.exists(STATIC_ROOT):
+    os.makedirs(STATIC_ROOT)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
