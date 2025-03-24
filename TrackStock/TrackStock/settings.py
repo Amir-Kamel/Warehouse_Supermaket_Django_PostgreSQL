bdfc9b019ba9d0120690541eb4facb47633152f5
amir-kamel <amir.kghabrial@gmail.com>
2025-03-24 02:59:08 +0200

    change allowed hosts

--- TrackStock/TrackStock/settings.py
+++ TrackStock/TrackStock/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-i7z2nwp&com&awl0g5ol6$it@%
 
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "warehousesupermaketdjangopost-production.up.railway.app",  # Add your Railway domain here
+]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Application definition
